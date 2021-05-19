8686f83c6a869672f9d44efaf071e4b0a292b4ba
Csabák Balázs <csabak.balazs@Csabak-Balazs-MacBook-Pro.local>
2021-05-20 01:27:00 +0200

    removed component

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -3,11 +3,7 @@
         Login
     </x-slot>
     <x-auth-card>
-        <x-slot name="logo">
-            <a href="/">
-                <x-application-logo class="w-20 h-20 fill-current text-gray-500" />
-            </a>
-        </x-slot>
+
 
         <!-- Session Status -->
         <x-auth-session-status class="mb-4" :status="session('status')" />
