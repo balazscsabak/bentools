73fdd7099c05dcb710a16d3c1460b4b31ce80c94
Balazs Csabak <balazs.csabak@gmail.com>
2021-09-18 17:18:34 +0200

    dev

--- resources/views/products/products-by-bategory.blade.php
+++ resources/views/products/products-by-bategory.blade.php
@@ -2,8 +2,8 @@
 
     <div class="products-page mt-3 pb-5">
         <div class="container">
-			<div class="row">
-				<div class="col-12 col-lg-8 align-self-center">
+			<div class="row justify-content-center">
+				<div class="col-12 col-lg-8">
 					<h5 class="mb-3">Kategória: {{ $slug ?? '' }}</h5>
 					<div class="row gy-3 related-items products-wrapper">
 						@foreach ($products as $product)
